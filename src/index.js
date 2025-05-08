73cccf7fb3a498dbcfc4531baa3ac91efe28d90b
Pasindu Dilshan <always.pasindu@gmail.com>
2025-05-08 16:26:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
-import { ThemeProvider } from "./context/ThemeContext"
-import { AuthProvider } from "./context/AuthContext"
-import { MovieProvider } from "./context/MovieContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <MovieProvider>
-            <App />
-          </MovieProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
